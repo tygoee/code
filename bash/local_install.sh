2d4a4f6f32da279bbf450bc58024524fe21bc166
tygoee <87303800+tygoee@users.noreply.github.com>
2023-10-05 18:23:22 +0200

    Change local_install.sh

--- bash/local_install.sh
+++ bash/local_install.sh
@@ -1,52 +1,90 @@
 #!/bin/bash
 shopt -s extglob # cp ./!()
 
+# Exit if there are no arguments
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <package-name>"
+    echo "Usage: $0 <package-name|args>"
     exit 1
 fi
 
-# Add the library paths to .profile if --setup is specified
-if [[ "$@" =- "--setup" ]]; then
-    echo "export LD_LIBRARY_PATH=$HOME/.local/lib:$HOME/.local/lib64:$HOME/.local/lib/x86_64-linux-gnu" >> $HOME/.profile
-    exit 0
-fi
+# Cycle through args
+for arg in "$@"; do
+    if [ "$arg" = "--help" ]; then
+        echo "General usage: $0 <package-name|args>"
+        exit 0
+    elif [ "$arg" = "--setup" ]; then
+        # Create all needed dirs if they dont exist
+        mkdir -p "$HOME/.local/lib/"
+        mkdir -p "$HOME/.local/lib64/"
+        mkdir -p "$HOME/.local/lib/x86_64-linux-gnu/"
+
+        echo "Created library directories..."
+
+        # Add the library path to .profile
+        # if --setup is specified
+        append_content="$(printf '%s' \
+            "export LD_LIBRARY_PATH=" \
+            "$HOME/.local/lib:" \
+            "$HOME/.local/lib64:" \
+            "$HOME/.local/lib/x86_64-linux-gnu")"
+
+        # Append to $HOME/.profile
+        # if it isn't already there
+        if ! grep -q "$append_content" \
+                "$HOME/.profile"; then
+            echo "Adding library paths..."
+            echo "$append_content" >> "$HOME/.profile"
+        else
+            echo "Library paths already"\
+                 "present, ignoring..."
+        fi
+
+        echo "Done."
+        exit 0
+    fi
+done
 
 # Make a temp dir and cd there
 package_name="$1"
 
-cd $HOME
+mkdir -p "$HOME/.temp/"
+cd "$HOME/.temp/" || { echo "Cannot CD into $HOME/.temp, exiting..." && exit 1; }
 
-mkdir -p .temp/
-cd .temp/
+# Exit if there's no internet connection
+if ! ping -c 1 -W 1 "deb.debian.org" > /dev/null 2>&1; then
+    echo "You need an active internet connection"
+    exit 1
+fi
 
 # Download package and all dependencies
-apt download $package_name
-dependencies=$(apt-cache depends -i "$package_name" | grep "Depends:" | awk '{print 2}')
+apt download "$package_name"
+dependencies=$(apt-cache depends -i "$package_name" | grep "Depends:" | awk '{print $2}')
 
 for dependency in $dependencies; do
     apt download "$dependency"
 done
 
 # Install all of them in .temp/install/
-for deb_file in .temp/*.deb; do
+for deb_file in "$HOME/.temp/"*.deb; do
     dpkg -x "$deb_file" "install/"
-    deb_file=$(echo "$deb_file" | sed 's:.*/::')
+    deb_file="${deb_file##*/}"
 
     echo "Installed $deb_file"
 done
 
-# Make all needed dirs if they dont exist
-mkdir -p $HOME/.local/lib/
-mkdir -p $HOME/.local/lib64/
-mkdir -p $HOME/.local/lib/x86_64-linux-gnu/
-
 # Cd into .temp/install/ and copy all files
 # to their corresponding directories
-cd install
-cp -r ./!(usr) $HOME/.local/  # > /dev/null
-cp -r ./usr/!(local) $HOME/.local  # > /dev/null
-cp -r ./usr/local/* $HOME/.local  # > /dev/null
+inst_dir="$HOME/.temp/install"
 
-# Remove the temp dir
-rm -r .temp/
+cp -r "$inst_dir"/!(usr) "$HOME/.local/"
+
+if [ -d usr/ ]; then
+    cp -r "$inst_dir/usr/"!(local) "$HOME/.local/"
+fi
+
+if [ -d usr/local/ ]; then
+    cp -r "$inst_dir/usr/local/"* "$HOME/.local/"
+fi
+
+# Remove the temp dir to clear up
+rm -r "$HOME/.temp/"
