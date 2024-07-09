6469ca2e3ef29d3ea201039b7560f1d811322a72
tygoee <tygoee@outlook.com>
2024-07-09 19:27:57 +0200

    Moved things around in a class in getimages.py

--- python/getimages/getimages.py
+++ python/getimages/getimages.py
@@ -10,108 +10,117 @@ from loadingbar import bar
 import csv
 
 
-async def download_image(
-        url: str, loadingbar: Generator[Any, Any, None]) -> Image.Image:
-    """
-    Download image specified with url.
+class Noordhoff:
+    def __init__(self, file: str):
+        for name, url in self.load_csv(file):
+            self.loadingbar: Generator[Any, Any, None]
+            self.name = name
+            self.url = url
 
-    :param url: The url where it downloads from
-    :param loadingbar: The generator function that updates the loading bar
-    """
+            self.jpgtopdf(asyncio.run(self.download_images()))
 
-    # Define the return_image lambda function
-    return_image: Callable[[str], Image.Image] = (
-        lambda url: Image.open(BytesIO(urlopen(url).read())))
-
-    # Download the image
-    result = await asyncio.to_thread(lambda: return_image(url))
-
-    # Update the loading bar
-    next(loadingbar)
-
-    # Return the image
-    return result
-
-
-async def noordhoff(name: str, base_url: str) -> list[Image.Image]:
-    """
-    Download images from noordhoff
-
-    :param base_url: The base url
-    """
-
-    print(f"Downloading {name} ({url}*.jpg)...")
-
-    def get_pages() -> int:
+    async def download_image(self, url: str) -> Image.Image:
         """
-        Gets the amount of pages with a binary search
+        Download image specified with url.
+
+        :param url: The url where it downloads from
         """
 
-        low, high = 1, 500
+        # Define the return_image lambda function
+        return_image: Callable[[str], Image.Image] = (
+            lambda url: Image.open(BytesIO(urlopen(url).read())))
 
-        while low < high:
-            mid = (low + high + 1) // 2
-            res: int
+        # Download the image
+        result = await asyncio.to_thread(lambda: return_image(url))
 
-            try:
-                res = urlopen(f"{base_url}{mid}.jpg").getcode()
-            except HTTPError as e:
-                res = e.code
+        # Update the loading bar
+        next(self.loadingbar)
 
-            if res == 404:
-                high = mid - 1
-            else:
-                low = mid
+        # Return the image
+        return result
 
-        return low
+    async def download_images(self) -> list[Image.Image]:
+        """
+        Download images from noordhoff
 
-    urls = [
-        f"{base_url}{x}.jpg"
-        for x in range(1, get_pages() + 1)
-    ]
+        :param base_url: The base url
+        """
 
-    loadingbar = bar(urls)
-    tasks = [download_image(url, loadingbar) for url in urls]
+        print(f"Downloading {self.name}... ({self.url}*.jpg)")
 
-    return await asyncio.gather(*tasks)
+        def get_pages() -> int:
+            """
+            Gets the amount of pages with a binary search
+            """
 
+            low, high = 1, 500
 
-def jpgtopdf(name: str, images: list[Image.Image]) -> None:
-    """
-    Converts jpg images to pdf files
+            while low < high:
+                mid = (low + high + 1) // 2
+                res: int
 
-    :param images: A list of Pillow images
-    """
+                try:
+                    res = urlopen(f"{self.url}{mid}.jpg").getcode()
+                except HTTPError as e:
+                    res = e.code
 
-    if not path.isdir('docs'):
-        mkdir('docs')
+                if res == 404:
+                    high = mid - 1
+                else:
+                    low = mid
 
-    pdf_path = path.join('.', 'docs', name + '.pdf')
+            return low
 
-    if len(images) < 1:
-        raise FileNotFoundError(f"{name} was not found")
+        urls = [
+            f"{self.url}{x}.jpg"
+            for x in range(1, get_pages() + 1)
+        ]
 
-    # Save all the images as a pdf in pdf_path
-    images[0].save(
-        pdf_path, 'PDF', resolution=100.0,
-        save_all=True, append_images=images[1:]
-    )
+        self.loadingbar = bar(urls)
+        tasks = [self.download_image(url) for url in urls]
 
-    print("Download complete.\n")
+        return await asyncio.gather(*tasks)
+
+    def jpgtopdf(self, images: list[Image.Image]) -> None:
+        """
+        Converts jpg images to pdf files
+
+        :param images: A list of Pillow images
+        """
+
+        # Create docs directory
+        if not path.isdir('docs'):
+            mkdir('docs')
+
+        pdf_path = path.join('.', 'docs', self.name + '.pdf')
+
+        if len(images) < 1:
+            raise FileNotFoundError(f"{self.name} was not found")
+
+        # Save all the images as a pdf in pdf_path
+        images[0].save(
+            pdf_path, 'PDF', resolution=100.0,
+            save_all=True, append_images=images[1:]
+        )
+
+        print("Download complete.\n")
+
+    def load_csv(self, file: str) -> Generator[tuple[str, str], None, None]:
+        with open(file, 'r') as fp:
+            next(fp)  # Skip the first line
+
+            data = csv.reader(fp)
+
+            for row in data:
+                name = row[5].replace('/', '-')  # For file names
+                ebookid = row[6] if not row[8] else row[8]
+                timestamp = row[7]
+
+                url = f"https://cdp.contentdelivery.nu/{ebookid}/" \
+                    f"{timestamp}/extract/assets/img/layout/"
+
+                yield name, url
 
 
 if __name__ == '__main__':
-    with open('Noordhoff.csv', 'r') as fp:
-        next(fp)
-
-        data = csv.reader(fp)
-
-        for row in data:
-            name = row[5].replace('/', '-')  # For file names
-            ebookid = row[6] if not row[8] else row[8]
-            timestamp = row[7]
-
-            url = f"https://cdp.contentdelivery.nu/{ebookid}/" \
-                f"{timestamp}/extract/assets/img/layout/"
-
-            jpgtopdf(name, asyncio.run(noordhoff(name, url)))
+    Noordhoff('Noordhoff.csv')
